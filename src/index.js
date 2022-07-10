c0251427c6ee361e79ce5d39a61e0924ad6bd827
harshmittal-cyber <you@example.com>
2022-07-10 18:50:10 +0530

    pwa added

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App from './App';
 import { Provider } from "react-redux";
 import { store, persistor } from "./redux/store"
 import { PersistGate } from 'redux-persist/integration/react'
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
@@ -17,5 +18,12 @@ root.render(
     </React.StrictMode>
   </Provider>
 );
+
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
+
 
 
